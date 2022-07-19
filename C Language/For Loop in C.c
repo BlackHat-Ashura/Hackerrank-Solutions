e7a7a7cba5e04426f3ea79a78e95f4944bbefef6
Hrudaya <78685572+BlackHat-Ashura@users.noreply.github.com>
2022-07-19 22:53:30 +0530

    Add files via upload

---
+++ C Language/For Loop in C.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main() 
+{
+    int a, b;
+    scanf("%d\n%d", &a, &b);
+    char* arr[] = {"one", "two", "three", "four", "five", "six", "seven",
+    "eight", "nine", "even", "odd"};
+    int index;
+    for (int i=a; i<=b; i++) {
+        index = i <= 9 ? i - 1 : 9 + i % 2;
+        printf("%s\n", arr[index]);
+    }
+    return 0;
+}
